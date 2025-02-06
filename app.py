36e93c8896471cb196322c800a4f782a69bc1efc
Gokul A G <160708777+GokulAG1608@users.noreply.github.com>
2025-02-06 16:29:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,6 @@ uploader_file = st.file_uploader("Choose an image",type=["jpg","jpeg","png"])
 image=''
 
 if uploader_file is not None:
-
     image = Image.open(uploader_file)
     st.image(image,caption = "Upload Image",use_column_width=True)
     
